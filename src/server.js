46501d4fbe519d9dbeaa28fcedc0ae050a2eb57b
Bernardo Marques <bernardomp.dev@gmail.com>
2023-05-10 16:36:15 -0300

    initial commit

--- src/server.js
+++ src/server.js
@@ -3,4 +3,4 @@ const app = require('./app');
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3001;
 
-app.listen(port, () => console.log('ouvindo porta', port));
+app.listen(port, () => console.log('ouvindo porta', port, '🟢'));
