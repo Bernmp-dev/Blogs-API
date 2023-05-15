723d69913a5c525ebc289371dd81e09c0fc7c88f
Bernardo Marques <bernardomp.dev@gmail.com>
2023-05-15 17:47:57 -0300

    feat:  camada service findOrCreateUser, findByEmail

--- src/migrations/20230510211428-users.js
+++ src/migrations/20230510211428-users.js
@@ -25,7 +25,6 @@ module.exports = {
       },
       image: {
         type: DataTypes.STRING,
-        allowNull: false
       },
     });
   },
--- src/services/user.js
+++ src/services/user.js
@@ -2,16 +2,34 @@ const sequelize = require('../transactions');
 
 const { User } = require('../models');
 
-const login = async ({ email, password }) => {
+const findOrCreateUser = async ({ displayName, email, password, image }) => {
   const result = await sequelize.transaction(async (t) => {
-    const user = await User.findOne({ where: { email, password } }, { transaction: t });
-    
+    const [user] = await User
+      .findOrCreate(
+        {
+          where: { email, password },
+          defaults: { displayName, image },
+          transaction: t,
+        },
+        );
     return user.id;
   });
 
   return result;
 };
 
+const findByEmail = async ({ email }) => {
+  const result = await sequelize.transaction(async (t) => {
+    const user = await User
+      .findOne({ where: { email } }, { transaction: t });
+    
+    return Boolean(user);
+  });
+
+  return result;
+};
+
 module.exports = {
-  login,
+  findOrCreateUser,
+  findByEmail,
 };
