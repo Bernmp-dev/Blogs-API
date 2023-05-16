b42882ab031988c66f8f019977131e98c8aefd5b
Bernardo Marques <bernardomp.dev@gmail.com>
2023-05-15 21:03:40 -0300

    feat: camada controller para listar categorias

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -9,6 +9,16 @@ const createCategory = async ({ body: category }, res) => {
   }
 };
 
+const listCategories = async (_req, res) => {
+  try {
+    const categoriesList = await categoryService.listCategories();
+    res.status(200).json(categoriesList);
+  } catch ({ message }) {
+    res.status(400).json({ message });
+  }
+};
+
 module.exports = {
   createCategory,
+  listCategories,
 };
